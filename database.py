254c3cea5fe3a25fbae26892412f8164a6c9886e
Ali Klemencic <aliklemencic@uchicago.edu>
2022-10-07 16:00:16 -0500

    updated to include topics and save even if fails

--- database.py
+++ database.py
@@ -8,69 +8,42 @@ import openstates_api
 from congress import Congress
 
 
-def get_state_data(state_names):
+def get_state_data(state, keywords):
     """
     Connects to the Open States API to get data on state bills.
     :param state_names: list of states
     :return: Pandas dataframe of bill data for all input states
     """
-    state_bills = openstates_api.get_data_for_all_states(state_names)
+    state_bills = openstates_api.gget_data_for_state_and_topic(state, keywords)
     state_bills_df = pd.DataFrame(state_bills)
 
     return state_bills_df
 
 
-def get_national_data(api_key):
-    """
-    Connects to the ProPublica API to get national House and Senate bill data.
-    :param api_key: (str) API key to connect
-    :return: Pandas dataframe of House and Senate bill data
-    """
-    congress = Congress(api_key)
-
-    house_bills = congress.bills.recent('house', congress=117)
-    house_df = pd.DataFrame(house_bills['bills'])
-
-    senate_bills = congress.bills.recent('senate', congress=117)
-    senate_df = pd.DataFrame(senate_bills['bills'])
-
-    national_bills_df = pd.concat([house_df, senate_df])
-    return national_bills_df
-
-
-def create_sql_tables(state_names, api_key):
+def create_sql_tables(state_names, keywords, topic):
     """
     Creates an SQL table from the state and national dataframes.
     :param state_names: list of states
     :param api_key: (str) API key to connect to ProPublica
     """
-    # this is currently not functional
-    conn = sqlite3.connect('bill_data.db')
+    database_name = topic + 'bill_data.db'
+    conn = sqlite3.connect(database_name)
     c = conn.cursor()
 
-    state_bills_df = get_state_data(state_names)
-    state_bills_df.to_sql("state_bills", conn)
-
-    national_bills_df = get_national_data(api_key)
-    national_bills_df.to_sql("national_bills", conn)
+    for state in state_names:
+        state_bills_df = get_state_data(state_names, keywords)
+        data_name = state + "_" + topic + "_bills"
+        state_bills_df.to_sql(data_name, conn)
+    
+    c.close()
 
 
 def run():
     """
-    Runs the above functions for all 50 states and our ProPublica API key.
+    Runs the above functions for Illinois and the US for climate change related bills.
     """
-    # not currently functional
-    state_names = ["Alaska", "Alabama", "Arkansas", "Arizona", "California",
-                   "Colorado", "Connecticut", "District of Columbia", "Delaware",
-                   "Florida", "Georgia", "Hawaii", "Iowa", "Idaho", "Illinois",
-                   "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts",
-                   "Maryland", "Maine", "Michigan", "Minnesota", "Missouri",
-                   "Mississippi", "Montana", "North Carolina", "North Dakota",
-                   "Nebraska", "New Hampshire", "New Jersey", "New Mexico",
-                   "Nevada", "New York", "Ohio", "Oklahoma", "Oregon",
-                   "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina",
-                   "South Dakota", "Tennessee", "Texas", "Utah", "Virginia",
-                   "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming"]
-    api_key = "j9Z6wFdBQgxMi7hmJHFPjILBN00g0MuEyowI3Ynm"
+    states = ["Illinois", "US"]
+    keywords = ["climate", "environment", "energy"]
+    topic = "climate_change"
 
-    create_sql_tables(state_names, api_key)
+    create_sql_tables(states, keywords, topic)
--- openstates_api.py
+++ openstates_api.py
@@ -13,29 +13,41 @@ class OpenStatesAPI:
     Class to connect to Open States via API.
     """
     base_path = "https://v3.openstates.org/bills?jurisdiction="
-    pages_path = "&sort=updated_desc&include=abstracts&include=actions&page="
+    query_path = "&sort=updated_desc&include=abstracts&include=actions&include=votes"
+    page_path = "&page="
     end_path = "&per_page=20&apikey=4a087065-961f-4547-b32e-f29ebf328083"
 
-    def __init__(self, state):
+    def __init__(self, state, keywords):
         """
         Constructor.
         :param state: (str) name of state
-        :param subjects: (list) list of subjects to filter by
+        :param keywords: list of keywords to filter by
+        :param loop_num: (int) number of loops to retrieve all bills
         """
         self.state = state.replace(" ", "%20")
+        self.keywords = keywords
         self.loop_num = 0
+        self.build_keyword_path()
+
+    def build_keyword_path(self):
+        """
+        Update the query_path to filter by the keywords given.
+        """
+        for keyword in self.keywords:
+            self.query_path += "&q=" + keyword.replace(" ", "%20")
 
     def get_loop_num(self):
         """
         Determine how many API calls are required to get all data.
         :return: (int) number of loops required
         """
-        r = requests.get(self.base_path + self.state + self.pages_path + '1' + self.end_path)
+        url = self.base_path + self.state + self.query_path + self.page_path + '1' + self.end_path
+        r = requests.get(url)
         if r.status_code != 200:
             raise ValueError("API call failed")
 
-        result = r.text.encode("iso-8859-1")
-        json_data = json.loads(result.decode('latin-1'))
+        result = r.text.encode()
+        json_data = json.loads(result.decode())
         count = json_data['pagination']['total_items']
         loop_num = math.ceil(count / 20)
         self.loop_num = loop_num
@@ -49,41 +61,38 @@ class OpenStatesAPI:
         """
         bills_list = []
         loop_num = self.get_loop_num()
-        count = 0
 
         for page in range(1, loop_num + 1):
-            count += 1
-            url = self.base_path + self.state + self.pages_path + str(page) + self.end_path
+            url = self.base_path + self.state + self.query_path + self.page_path + str(page) + self.end_path
             r = requests.get(url)
             if r.status_code != 200:
-                raise ValueError("API call failed")
+                return bills_list
+                #raise ValueError("API call failed. " + r.reason)
                 # this happens a lot due to call number limits and restricts the ways in which we can make the calls
                 # will need to rethink how we store data and make API calls
 
-            result = r.text.encode("iso-8859-1")
-            json_data = json.loads(result.decode('latin-1'))
+            result = r.text.encode()
+            json_data = json.loads(result.decode())
             bills_list.append(json_data['results'])
 
-            print(str(count) + " of " + str(self.loop_num) + " loops complete.")
+            print(str(page) + " of " + str(self.loop_num) + " calls complete.")
             # added in to try to get around API call limits
             time.sleep(1)
 
         return bills_list
 
 
-def get_data_for_all_states(states):
+def get_data_for_state_and_topic(state, keywords):
     """
-    Function to make an API call to Open States
-    and get data for every state passed in.
-    :param states: list of states
+    Function to make an API call to Open States based on the state and keywords given.
+    :param state: (str) state
+    :param keywords: list of keywords
     :return: list of all bill dictionaries
     """
     all_bills = []
 
-    for state in states:
-        api_call = OpenStatesAPI(state)
-        bills = api_call.get_data()
-        all_bills += bills
+    api_call = OpenStatesAPI(state, keywords)
+    bills = api_call.get_data()
+    all_bills += bills
 
     return all_bills
-
